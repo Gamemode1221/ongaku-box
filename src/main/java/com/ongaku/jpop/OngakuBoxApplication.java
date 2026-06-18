5c7b916b60d9e9f6ebb15ea6ff9f53691a17e2da
Gamemode1221 <yongho1221@gmail.com>
2026-06-18 12:49:49 +0900

    first commit

---
+++ src/main/java/com/ongaku/jpop/OngakuBoxApplication.java
@@ -0,0 +1,13 @@
+package com.ongaku.jpop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OngakuBoxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OngakuBoxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ongaku/jpop/OngakuBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ongaku.jpop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OngakuBoxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
